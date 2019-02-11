3be078df95cb9d77ee8f28edcd423bc03c83be68
Thompson Plyler <nealok.g@gmail.com>
2019-02-11 15:44:27 -0500

    JWT auth functionality.

--- test.js
+++ test.js
@@ -1,7 +1,13 @@
 let tst_btn = document.getElementById("test")
+let lgout_btn = document.getElementById("logout")
+let check_btn = document.getElementById("check")
 tst_btn.addEventListener("click", test)
+lgout_btn.addEventListener("click",logout)
+check_btn.addEventListener("click", check)
 
-function test(){
+
+function test(event){
+    event.preventDefault()
     fetch("http://localhost:3000/api/v1/users/", {
         method: "POST",
         headers: {
@@ -16,6 +22,17 @@ function test(){
         }
 
         )
-    }).then(r=>r.json()).then(obj=>console.log(obj)).catch(err=>console.log(err))
+    }).then(r=>r.json()).then(obj=>{
+        window.sessionStorage.accessToken = obj.jwt
+        console.log(obj.jwt)}).catch(err=>console.log(err))
     console.log("Button clicked.")
+}
+
+function logout(event){
+    event.preventDefault()
+    window.sessionStorage.clear()
+}
+
+function check(){
+    console.log(window.sessionStorage.valueOf())
 }
